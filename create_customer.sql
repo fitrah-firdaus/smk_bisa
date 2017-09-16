aede06e0cb0530c4f663520bbd7925e4e52d0130
FEF <fitrah.firdaus@traveloka.com>
2017-09-16 09:52:00 +0700

    add file customer

---
+++ create_customer.sql
@@ -0,0 +1,7 @@
+CREATE TABLE customer (
+customer_id BIGSERIAL,
+customer_name varchar(50),
+customer_address TEXT,
+customer_email varchar(50),
+identity_number varchar(50),
+customer_phone_number varchar(12));
